8eb327648be3232f5bd84908cc9ad9505b2e66c8
Ekaterina <katya.kinal@gmail.com>
2022-08-02 19:47:32 +0300

    New Changes

--- Ex004/Program.cs
+++ Ex004/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-double numberA = 12;
-double numberB = 5;
+int numberA = new Random().Next(1,10); // вещественный тип данных
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
 Console.WriteLine(numberA / numberB);
