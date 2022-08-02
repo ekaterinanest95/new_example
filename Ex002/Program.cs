81f148485b3766c9be32381674e5556efcf80af4
Ekaterina <katya.kinal@gmail.com>
2022-08-02 13:00:25 +0300

    Ex002

---
+++ Ex002/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine()!;
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Я так ждала тебя, Маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
